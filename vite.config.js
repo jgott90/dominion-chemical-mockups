8caa5e488cf01a04711ce69801ca7c9fe0bf6b63
jgott90 <jgott90@gmail.com>
2025-05-25 16:10:54 -0400

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projects/dominion-chemical/',  // <-- This line makes all assets load from the correct subfolder!
   plugins: [react()],
   server: {
-    open: true,  // Automatically open the site on launch
+    open: true, // Automatically open the site on launch
   },
   css: {
     postcss: './postcss.config.js',
   },
 })
-
-
