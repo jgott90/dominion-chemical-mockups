b6820236533828980b5550c15010aeaf6aeb9ed5
jgott90 <jgott90@gmail.com>
2025-05-29 15:02:35 -0400

    Prepare for deployment to GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { HeadProvider } from "react-head";
 import App from "./App";
 import { SearchProvider } from "./context/SearchContext";
@@ -8,12 +8,12 @@ import "./styles/index.css"; // Ensure your global CSS is imported here
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/">
+    <HashRouter basename="/">
       <HeadProvider>
         <SearchProvider>
           <App />
         </SearchProvider>
       </HeadProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',  // <-- This line makes all assets load from the correct subfolder!
+  base: '/dominion-chemical-mockups/',  // <-- This line makes all assets load from the correct subfolder!
   plugins: [react()],
   server: {
     open: true, // Automatically open the site on launch
